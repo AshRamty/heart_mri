33ff5398b5ca9354fce6d45c5ec931fcec162958
Ashwini Ramamoorthy <ashwinir@dawn12.stanford.edu>
2019-03-22 18:12:00 -0700

    updated hyperparameters

--- scripts/run_mr_pretrained.sh
+++ scripts/run_mr_pretrained.sh
@@ -2,7 +2,7 @@
 
 # SEEDS ="0 14 57 1234"
 
-python mr_train_pw.py --seed 0 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed1 -E 20 --pretrained_model_path oc_checkpoints_pw_1
-python mr_train_pw.py --seed 14 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed2 -E 20 --pretrained_model_path oc_checkpoints_pw_1
-python mr_train_pw.py --seed 57 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed3 -E 20 --pretrained_model_path oc_checkpoints_pw_1
-python mr_train_pw.py --seed 1234 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed4 -E 20 --pretrained_model_path oc_checkpoints_pw_1
+python mr_train_pw.py --seed 0 --data_seed 0 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed1 -E 20 --pretrained_model_path oc_checkpoints_pw_1
+python mr_train_pw.py --seed 14 --data_seed 14 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed2 -E 20 --pretrained_model_path oc_checkpoints_pw_1
+python mr_train_pw.py --seed 57 --data_seed 57 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed3 -E 20 --pretrained_model_path oc_checkpoints_pw_1
+python mr_train_pw.py --seed 1234 --data_seed 1234 --train /lfs/1/heartmri/mr/train --dev /lfs/1/heartmri/mr/dev --test /lfs/1/heartmri/mr/test --checkpoint_dir mr_checkpoints_pw_seed4 -E 20 --pretrained_model_path oc_checkpoints_pw_1
