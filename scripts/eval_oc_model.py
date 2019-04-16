73ee2f06dd0977679d6e6ae2219b226a29ac309c
AshRamty <ashwini.ramty@gmail.com>
2019-04-16 01:14:36 -0700

    added model to train on temporal models

--- scripts/eval_oc_model.py
+++ scripts/eval_oc_model.py
@@ -1,18 +1,20 @@
 '''
-Runs supervised learning on MR data
-Does not include hyperparameter tuning
-Uses pretrained weights from open/close training 
-
+Script to load a saved OC model
+Evaluate it on dev / test set or a new dataset
 '''
 import sys
+import os
 sys.path.append('../metal')
 sys.path.append('../heart-MRI-pytorch')
 sys.path.append('../data')
 
+import pickle
 import numpy as np
 import argparse
-import pickle
 import torch
+import time
+import logging
+import warnings
 import pandas
 import torchvision
 import torch.nn as nn
@@ -21,13 +23,18 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 
+from dataloader_4ch import UKBB_LAX_Roll
+from models.frame.densenet_av import densenet_40_12_bc
+from utils import *
+from frame_encoder import FrameEncoderOC
+
+from metal.label_model import LabelModel
+from metal.label_model.baselines import MajorityLabelVoter
 from metal.end_model import EndModel
 from metal.contrib.modules import Encoder, LSTMModule
+from metal.analysis import lf_summary, confusion_matrix
 
-from dataloader_4ch import UKBB_LAX_MR
-from frame_encoder import FrameEncoderOC
 from sampler import ImbalancedDatasetSampler
-
 from utils import *
 from metrics import *
 from dataloaders import *
@@ -40,84 +47,82 @@ except ImportError:
 	# for python3
 	import _pickle as cPickle
 
-# load dataset
-def load_dataset(args):
+logger = logging.getLogger(__name__)
+
+
+def read_labels(label_list):
+	L = []
+	for index in range(len(label_list)):
+		L.append(np.load(label_list[index]))
+
+	L = np.squeeze(np.array(L))
+	
+	# reshaping array from (PID,frames,) -> (PID*frames,)
+	m = L.shape[0]
+	n = L.shape[1]
+	if(len(L.shape) == 2): # true labels 
+		L = np.reshape(L,(m*n,))
+		L = L+1 # changing from 0-indexing to 1-indexing
+	else:
+		L = csr_matrix(np.reshape(L,(m*n,L.shape[2])))
+
+	return L
+
+
+def load_labels(args):
+	#path = os.getcwd()
+	L = {}
+	Y = {}
+
+	#train_lf_list = glob(args.train + '/lf_labels/*.npy') 
+	L["train"] = read_labels(glob(args.train + '/lf_labels/*.npy'))
+	L["dev"] = read_labels(glob(args.dev + '/lf_labels/*.npy'))
+	L["test"] = read_labels(glob(args.test + '/lf_labels/*.npy'))
+
+	#import ipdb; ipdb.set_trace()
+	Y["dev"] = read_labels(glob(args.dev + '/true_labels/*.npy'))
+	Y["test"] = read_labels(glob(args.test + '/true_labels/*.npy'))	
+
+	return L,Y
+
+
+def load_dataset_eval(args,Ydev,Ytest):
 	'''
 	Loading LAX 4ch data
 	'''
-	DataSet = UKBB_LAX_MR
-	train = DataSet(args.train, args.mask, seed=args.data_seed)
-	dev = DataSet(args.dev, args.mask, seed=args.data_seed)
+	DataSet = UKBB_LAX_Roll
+	#train = DataSet(args.train, Ytrain, seed=args.data_seed, mask = args.mask)
+	#dev = DataSet(args.dev, np.expand_dims(Ydev,1), seed=args.data_seed)
+	dev = DataSet(args.dev, Ydev, seed=args.data_seed, mask = args.mask)
+
 	if args.test:
-		test = DataSet(args.test, args.mask, seed=args.data_seed)
+		#test = DataSet(args.test, np.expand_dims(Ytest,1), seed=args.data_seed)
+		test = DataSet(args.test, Ytest, seed=args.data_seed, mask = args.mask)
 	else:
 		test = None
 
-	return train, dev, test
+	return dev, test
 
-# dataloader
-def get_data_loader(train, dev, test=None, batch_size=4, num_workers=1):
+
+# dataloader 
+def get_data_loader_eval(dev, test=None, batch_size=4, num_workers=1):
 	
 	data_loader = {}
 	#data_loader["train"] = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-	data_loader["train"]  = DataLoader(train,sampler=ImbalancedDatasetSampler(train), batch_size=batch_size, num_workers=num_workers)
+	#train_loader = DataLoader(train,sampler=ImbalancedDatasetSampler(train), batch_size=batch_size, num_workers=num_workers)
 	data_loader["dev"]   = DataLoader(dev, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 	data_loader["test"]  = None if not test else DataLoader(test, batch_size=batch_size,shuffle=False, num_workers=num_workers)
 
 	return data_loader
 
 
-def set_init_kwargs(args):
-	# didn't save init kwargs - to update the oc_cnn_lstm code to save it
-	hidden_size = 128 
-	num_classes = 2
-	encode_dim = 1000
-
-	cnn_encoder = FrameEncoderOC
-
-	if(torch.cuda.is_available()):
-		device = 'cuda'
-	else:
-		device = 'cpu'
-	
-	# Define LSTM module
-	lstm_module = LSTMModule(
-		encode_dim,
-		hidden_size,
-		bidirectional=False,
-		verbose=False,
-		lstm_reduction="attention",
-		encoder_class=cnn_encoder,
-		encoder_kwargs = {"requires_grad":args.requires_grad}
-		)
-
-	init_kwargs = {
-	"layer_out_dims":[hidden_size, num_classes],
-	"input_module": lstm_module, 
-	"optimizer": "adam",
-	"verbose": False,
-	"input_batchnorm": False,
-	"use_cuda":torch.cuda.is_available(),
-	'checkpoint_dir':args.checkpoint_dir,
-	'seed':args.seed,
-	'device':device}
-
-	return init_kwargs
-
-
 def load_model_snapshot(args,inputdir):
 	"""
 	Load 
 	"""
-	#import ipdb; ipdb.set_trace();
 
-	# if frame encoder weights to be frozen for MR, finetuned over open close
-	if(args.requires_grad == False):
-		init_kwargs = set_init_kwargs(args)
-	else:  # if frame encoder weights to be finetuned for MR and finetuned over open close 
-		init_kwargs = pickle.load(open(f'{inputdir}/init_kwargs.pickle', "rb"))
-		init_kwargs["seed"] = args.seed
-
+	init_kwargs = pickle.load(open(f'{inputdir}/init_kwargs.pickle', "rb"))
+	init_kwargs["seed"] = args.seed
 
 	model = EndModel(**init_kwargs)
 	map_location = 'gpu' if torch.cuda.is_available() else 'cpu'
@@ -130,56 +135,37 @@ def load_model_snapshot(args,inputdir):
 
 	return model
 
-# def train_model(args):
-def train_model(args):
+def eval_model(args):
 
+    #global args
+    #args = parser.parse_args()
+
+	hidden_size =128 
+	num_classes = 2
+	encode_dim = 1000 # using get_frm_output_size()
+
+	L,Y = load_labels(args) 
+
+	# Label Model
+	# labelling functions analysis
+	print(lf_summary(L["dev"], Y = Y["dev"]))
+
+	# End Model
 	# Create datasets and dataloaders
-	train, dev, test = load_dataset(args)
-	#print('train size:',len(train)) # 250
-	#print('dev size:',len(dev)) # 250
-	#print('test size:',len(test)) # 250
-	# data in tuple of the form (series,label)
-	# series shape (50,3,224,224)
+	dev, test = load_dataset_eval(args, Y["dev"], Y["test"])
+	data_loader = get_data_loader_eval(dev, test, args.batch_size, args.num_workers)
+	#print(len(data_loader["train"])) # 18850 / batch_size
+	#print(len(data_loader["dev"])) # 1500 / batch_size
+	#print(len(data_loader["test"])) # 1000 / batch_size 
+	#import ipdb; ipdb.set_trace()
 
-	data_loader = get_data_loader(train, dev, test, args.batch_size)
-
-	if(torch.cuda.is_available()):
-		device = 'cuda'
-	else:
-		device = 'cpu'
-
+	# load model 
 	model = load_model_snapshot(args,args.pretrained_model_path)
-	#import pdb; pdb.set_trace()
 
-	dropout = 0.4
-	# Train end model
-	model.train_model(
-		train_data=data_loader["train"],
-		valid_data=data_loader["dev"],
-		l2=args.weight_decay,
-		lr=args.lr,
-		n_epochs=args.n_epochs,
-		log_train_every=1,
-		verbose=True,
-		progress_bar = True,
-		loss_weights = [0.9,0.1],
-		batchnorm = 'False',
-		log_valid_metrics = ['accuracy','f1'],
-		checkpoint_metric = 'f1',
-		checkpoint_dir = args.checkpoint_dir,
-		#validation_metric='accuracy',
-		#input_dropout = 0.1,
-		middle_dropout = dropout,
-		)
-
-
-	model.score(data_loader["dev"], verbose=True, metric=['accuracy', 'precision', 'recall', 'f1','roc-auc'])
-	# Test end model 
-	'''
-	if(test_loader != None):
-		end_model.score(test_loader, verbose=True, metric=['accuracy', 'precision', 'recall', 'f1'])
-
-	'''
+	# evaluate end model
+	model.score(data_loader["dev"], verbose=True, metric=['accuracy','precision', 'recall', 'f1'])
+	#end_model.score((Xtest,Ytest), verbose=True, metric=['accuracy','precision', 'recall', 'f1'])
+	
 
 
 if __name__ == "__main__":
@@ -204,13 +190,13 @@ if __name__ == "__main__":
 	argparser.add_argument("--momentum", default=0.9, type=float, help="momentum")
 	argparser.add_argument("--weight-decay","--wd",default=1e-4,type=float,help="weight decay (default: 1e-4)")
 	argparser.add_argument("-E", "--n_epochs", type=int, default=1, help="number of training epochs")
-
 	argparser.add_argument("--seed",type=int,default=123,help="random seed for initialisation")
-	argparser.add_argument("--mask",type=str,default=False,help="Selects whether to use segmented data")
-	argparser.add_argument("--checkpoint_dir", type=str, default="mr_checkpoints", help="dir to save checkpoints")
-	argparser.add_argument("--pretrained_model_path", type=str, default="oc_checkpoints_pw", help="dir of the best pretrained model")
+	argparser.add_argument("--lstm_reduction",type=str,default="attention",help="LSTM reduction at output layer")
 
-	argparser.add_argument("--requires_grad", type=bool, default=False, help="Selects whether to freeze or finetune frame encoder")
+	argparser.add_argument("--mask",type=bool,default=False,help="Selects whether to use segmented data")
+	#argparser.add_argument("--checkpoint_dir", type=str, default="oc_checkpoints", help="dir to save checkpoints")
+	#argparser.add_argument("--requires_grad", type=bool, default=False, help="Selects whether to freeze or finetune frame encoder")
+	argparser.add_argument("--pretrained_model_path", type=str, default="oc_checkpoints_all/oc_checkpoints_pw", help="dir of the best pretrained model")
 
 	args = argparser.parse_args()
 
@@ -229,4 +215,6 @@ if __name__ == "__main__":
 	logger.info("python " + " ".join(sys.argv))
 	print_key_pairs(args.__dict__.items(), title="Command Line Args")
 
-	train_model(args)
+	eval_model(args)
+
+
