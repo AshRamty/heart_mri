692f20d1f9c6c574024c8edae78c7f48b1f073e6
Ashwini Ramamoorthy <ashwinir@dawn1.stanford.edu>
2019-06-27 12:05:41 -0700

    converting to package

---
+++ frame_encoder/_init_.py
@@ -0,0 +1,3 @@
+from .frame_encoder import FrameEncoderBAV
+from .frame_encoder import FrameEncoderOC
+from .frame_encoder import FrameEncoderOCDense
--- mr/mr_train.py
+++ mr/mr_train.py
@@ -4,9 +4,13 @@ Does not include hyperparameter tuning
 
 '''
 import sys
+#sys.path.append('../')
 sys.path.append('../metal')
 sys.path.append('../heart-MRI-pytorch')
 sys.path.append('../data')
+sys.path.append('../dataloaders')
+sys.path.append('../frame_encoder')
+sys.path.append('../utils')
 
 import numpy as np
 import argparse
@@ -22,10 +26,11 @@ from torch.utils.data import Dataset, DataLoader
 from metal.end_model import EndModel
 from metal.contrib.modules import Encoder, LSTMModule
 
+import ipdb; ipdb.set_trace()
 #import metal.contrib.modules.resnet_cifar10 as resnet
 #from dataloaders.ukbb import UKBBCardiacMRI
 #from models.frame.densenet_av import DenseNet3, densenet_40_12_bc
-from dataloader.dataloader_4ch import UKBB_LAX_MR
+from dataloaders.dataloader_4ch import UKBB_LAX_MR
 from frame_encoder.frame_encoder import FrameEncoderOC
 from utils.sampler import ImbalancedDatasetSampler
 
@@ -168,7 +173,7 @@ if __name__ == "__main__":
 
 	argparser.add_argument("--seed",type=int,default=123,help="random seed for initialisation")
 	argparser.add_argument("--mask",type=str,default=False,help="Selects whether to use segmented data")
-	argparser.add_argument("--checkpoint_dir", type=str, default="mr_checkpoints", help="dir to save checkpoints")
+	argparser.add_argument("--checkpoint_dir", type=str, default="mr_checkpoints/test", help="dir to save checkpoints")
 	argparser.add_argument("--preprocess", type=bool, default=True, help="Selects whether to apply preprocessing (histogram equalization) to data")
 	
 	args = argparser.parse_args()
